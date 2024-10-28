1a3e11c75e5cc492a2630773462963464b40b1a3
Eric Rodgers <ejrodgers8694@gmail.com>
2024-10-28 11:53:44 -0500

    Create routes for sent messages through POST

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+## Controller for CryptoChat
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__) 
+
+messages = [] ## stores messsages from chat.html message here temporarily
 
 @app.route('/') ## route decorator for home page
 def home():
@@ -8,7 +12,14 @@ def home():
 
 @app.route('/chat')
 def chat():
-  return render_template('chat.html')
+  return render_template('chat.html', messages = messages) ## passess all messages into the message display
+
+@app.route('/send', methods=['POST']) ## activated when a message is sent through POST to /send
+def send_message():
+  message = request.form.get('message') ## retrieves messages from form in chat.html
+  if message:
+    messages.append(message) # stores message in temporary array
+  return redirect(url_for('chat'))
 
 if __name__ == '__main__': ## used if file is run directly
   app.run(debug=True) ## starts server/if website alters, reloads 
