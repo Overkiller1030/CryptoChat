485b2cd9de5b14618a1f143464d7d651368366b6
Eric Rodgers <ejrodgers8694@gmail.com>
2024-10-28 02:39:24 -0500

    Add route to ChatRoom page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
+@app.route('/chat')
+def chat():
+  return render_template('chat.html')
+
 if __name__ == '__main__': ## used if file is run directly
   app.run(debug=True) ## starts server/if website alters, reloads 
