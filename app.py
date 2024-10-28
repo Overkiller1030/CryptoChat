1f8cb4259651a9d981b16dbcab794ce0f4133819
Eric Rodgers <ejrodgers8694@gmail.com>
2024-10-28 12:55:06 -0500

    Add functionality for creating shared secret

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ## Controller for CryptoChat
 
 from flask import Flask, render_template, request, redirect, url_for
+from crypto_utils import generateKeyPair, serializePublicKey, deserializePublicKey, getSharedSecret # Utilizes crypto_utils.py
 
 app = Flask(__name__) 
 
@@ -22,4 +23,21 @@ def send_message():
   return redirect(url_for('chat'))
 
 if __name__ == '__main__': ## used if file is run directly
+
+  # Testing shared secret exchange
+  privateKey1, publicKey1 = generateKeyPair()
+  privateKey2, publicKey2 = generateKeyPair()
+
+  serializePublicKey1 = serializePublicKey(publicKey1)
+  serializePublicKey2 = serializePublicKey(publicKey2)
+
+  deserializePublicKey1 = deserializePublicKey(serializePublicKey1)
+  deserializePublicKey2 = deserializePublicKey(serializePublicKey2)
+
+  sharedSecret1 = getSharedSecret(privateKey1, deserializePublicKey2)
+  sharedSecret2 = getSharedSecret(privateKey2, deserializePublicKey1)
+
+  assert sharedSecret1 == sharedSecret2, "Shared Secrets are not the same!"
+  print("Shared secret established successfully!")
+
   app.run(debug=True) ## starts server/if website alters, reloads 
